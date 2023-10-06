934d37d1917064c7792185c378589a49b8a16528
ignacy <ignacpowala@gmail.com>
2023-10-06 21:24:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,14 +6,10 @@ const selectBtn = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
-// selectBtn.classList.add('is-hidden');
 loader.classList.replace('loader', 'is-hidden');
 error.classList.add('is-hidden');
-// catInfo.classList.add('is-hidden');
 let arrayBreedsId = [{ placeholder: true, text: '' }];
 const breedSelect = () => {
-  // loader.classList.replace('loader', 'is-hidden');
-  // selectBtn.classList.remove('is-hidden');
   fetchBreeds()
     .then(data => {
       data.forEach(element => {
@@ -25,7 +21,6 @@ const breedSelect = () => {
       });
     })
     .catch(onFetchError);
-  // .finally(() => loader.classList.add('is-hidden'));
 };
 document.addEventListener('DOMContentLoaded', breedSelect);
 function onSelectBreed(event) {
@@ -37,7 +32,6 @@ function onSelectBreed(event) {
     fetchCatByBreed(breedId)
       .then(data => {
         loader.classList.replace('loader', 'is-hidden');
-        // select.classList.remove('is-hidden');
         const { url, breeds } = data[0];
         catInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
         catInfo.classList.remove('is-hidden');
@@ -48,8 +42,6 @@ function onSelectBreed(event) {
 }
 selectBtn.addEventListener('change', onSelectBreed);
 function onFetchError(error) {
-  // selectBtn.classList.add('is-hidden');
-  // loader.classList.add('is-hidden');
   Notiflix.Notify.failure(
     'Oops! Something went wrong! Try reloading the page!',
     {
